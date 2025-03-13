5cfdef58d0630598cdadb4f3a2cfe3fb6e0b868a
Kit <nikita.khaziev@mail.ru>
2025-03-13 21:00:07 +0300

    dsd

--- frontend/plants/src/App.tsx
+++ frontend/plants/src/App.tsx
@@ -3,9 +3,6 @@ function App() {
 
   return (
     <div className="bg-blue-100 w-screen h-screen">
-      <p>dsdsdsdsdsdsd</p>
-        <button>RED</button>
-        <p>qwerty</p>
     </div>
   )
 }

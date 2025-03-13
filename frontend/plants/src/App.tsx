ccf20d6a4f2197b832d471223c674d66b04bb2dd
Kit <nikita.khaziev@mail.ru>
2025-03-13 21:17:48 +0300

    sds

--- frontend/plants/src/App.tsx
+++ frontend/plants/src/App.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header.tsx";
+
 function App() {
 
 
   return (
     <div className="m-0 pt-[25px] pr-[120px] pl-[120px] pb-[0]">
-      <p>dsdsdsdsdsdsd</p>
-
+        <Header />
     </div>
   )
 }
--- frontend/plants/src/components/Header.tsx
+++ frontend/plants/src/components/Header.tsx
@@ -1,10 +1,18 @@
-
-
+import ImgLogo from "../assets/Header/Logo.svg"
+import ImgSearch from "../assets/Header/Search.svg"
 export default function Header() {
     return (
-        <div>
-            <label htmlFor="text">Text</label>
-            <input type="text"/>
-        </div>
+        <header className="flex flex-row justify-between">
+            <img src={ImgLogo} alt="Logo" className="w-[150px] h-[34px]" />
+            <div className="flex flex-row gap-[50px]">
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">Plant Care</a>
+                <a href="#">Blogs</a>
+            </div>
+            <div>
+                <a href="#"><img src={ImgSearch} alt=""/></a>
+            </div>
+        </header>
     )
 }

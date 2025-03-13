f193535a82e766c7e2d7cf593567001a6642cf34
Kit <nikita.khaziev@mail.ru>
2025-03-13 15:45:16 +0300

    dd

--- frontend/plants/src/App.tsx
+++ frontend/plants/src/App.tsx
@@ -2,7 +2,9 @@ function App() {
 
 
   return (
-    <div className="bg-blue-100 w-screen h-screen"></div>
+    <div className="bg-blue-100 w-screen h-screen">
+      <p>dsdsdsdsdsdsd</p>
+    </div>
   )
 }
 

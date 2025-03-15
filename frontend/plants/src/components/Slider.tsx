94d421e7345db7109e10a0e0b29d20f4130ca6a4
Kit <nikita.khaziev@mail.ru>
2025-03-15 21:59:01 +0300

    Второй слайд добавлен

--- frontend/plants/src/components/Slider.tsx
+++ frontend/plants/src/components/Slider.tsx
@@ -1,4 +1,6 @@
 import ImgPlants1Modal from "../assets/Slider/plants-modal.png"
+import ImgPlants2Modal from "../assets/Slider/BookPlant.png"
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {Pagination, A11y, Autoplay} from 'swiper/modules';
 // @ts-ignore
@@ -17,13 +19,14 @@ export default function Slider() {
                 modules={[Pagination, A11y, Autoplay]}
                 spaceBetween={50}
                 slidesPerView={1}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,}}
+                // autoplay={{
+                //     delay: 6000,
+                //     disableOnInteraction: false,}}
                 pagination={{clickable: true}}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
+                {/*1*/}
                 <SwiperSlide>
                     <div className="flex justify-between content-center h-[450px] w-full">
                         <div className="flex flex-col w-[50%] gap-[7px] pt-[60px] pl-[40px]">
@@ -50,10 +53,32 @@ export default function Slider() {
                     </div>
                 </SwiperSlide>
 
+                {/*2*/}
+                <SwiperSlide>
+                    <div className="flex justify-between content-center h-[450px] w-full">
+                        <div className="flex flex-col w-[50%] gap-[7px] pt-[60px] pl-[40px]">
+                            <p className="text-[14px] text-gray-800 font-normal leading-[16px] uppercase">Welcome to
+                                GreenShop</p>
+                            <h1 className="text-[70px] text-gray-900 font-extrabold leading-[77px] uppercase">
+                                did you know?
+                            </h1>
+                            <p className="text-[14px] leading-[30px] font-normal text-gray-500">
+                                Plants are more fascinating than you think! Discover amazing facts about how they breathe,
+                                grow, and even communicate. Dive into the green world and learn something new today!
+                            </p>
 
-                <SwiperSlide>Slide 2</SwiperSlide>
+                            <button className="uppercase bg-green-700 text-white w-[140px] h-[40px] mt-[40px] rounded-[8px] hover:bg-green-600
+                                    cursor-pointer transition duration-300 font-semibold">
+                                Read more
+                            </button>
+                        </div>
+                        <div className="w-[44%] flex flex-column justify-end items-end">
+                            <img src={ImgPlants2Modal} alt="Img" className="h-[80%]"/>
+                        </div>
+                    </div>
+                </SwiperSlide>
 
-
+                {/*3*/}
                 <SwiperSlide>Slide 3</SwiperSlide>
             </Swiper>
             <style>

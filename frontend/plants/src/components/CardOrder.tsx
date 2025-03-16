0067abe0f6ffe543a0838951f6750abfb4feb72a
Kit <nikita.khaziev@mail.ru>
2025-03-16 19:31:22 +0300

    Обновление2

--- frontend/plants/src/components/CardOrder.tsx
+++ frontend/plants/src/components/CardOrder.tsx
@@ -1,9 +1,18 @@
 
 
-export default function CardOrder() {
-    return (
-        <div>
+interface  ProductCardProps {
+    image: string;
+    title: string;
+    price: number;
+}
 
+
+const CardOrder: React.FC<ProductCardProps> = ({image,title,price}) => {
+    return (
+        <div className="w-[258px] h-[352px]">
+            <div className="w-full h-[300px] bg-gray-100">
+                <img src={image} alt={title} className="w-full"/>
+            </div>
         </div>
     )
 }

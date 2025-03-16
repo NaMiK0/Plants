dbb1936bd5824da4854058b94ff78904ffe3ed40
Eduard <eduard.agishev.2005@mail.ru>
2025-03-16 21:13:43 +0300

    Авторизация

--- frontend/plants/src/components/Header.tsx
+++ frontend/plants/src/components/Header.tsx
@@ -2,11 +2,12 @@ import ImgLogo from "../assets/Header/Logo.svg"
 import ImgSearch from "../assets/Header/search.png"
 import ImgBasket from "../assets/Header/basket.png"
 import ImgLogout from "../assets/Header/Logout.svg"
-import ImgX from "../assets/Header/X.svg"
 import ImgGoogle from "../assets/Header/Google.png"
 import ImgFacebook from "../assets/Header/Facebook.png"
 import ImgLine1 from "../assets/Header/Line 7.png"
 import ImgLine2 from "../assets/Header/Line 8.png"
+import ImgHide from "../assets/Header/Hide.png"
+import ImgShow from "../assets/Header/Show.png"
 import Modal from "./Modal.tsx";
 import {useState} from "react";
 
@@ -23,6 +24,11 @@ export default function Header() {
     const closeModal = () => {
         setIsModalOpen(false);
     }
+
+    const [isRegistering, setIsRegistering] = useState(false);
+
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
     return (
         <>
@@ -43,34 +49,87 @@ export default function Header() {
                         <img src={ImgLogout} alt="Logout" className="w-[20px] h-[20px] mt-[3px]"/> Login
                     </button>
 
-                    <Modal isOpen={isOpenModal} closeModal={closeModal}>
-                        <button onClick={closeModal}
-                                className="absolute right-[480px] top-[115px] ">
-                            <img src={ImgX} alt="X"/></button>
+                    <Modal isOpen={isOpenModal} closeModal={closeModal} isRegistering={isRegistering}>
                         <div className="flex flex-row justify-center content-center gap-[30px] pt-[30px]">
-                            <a href="#" className="font-medium text-[20px] leading-[16px]">Login</a>
+                            <a
+                                href="#"
+                                className={`font-medium text-[20px] leading-[16px] ${!isRegistering ? "text-[#46A358]" : ""}`}
+                                onClick={(e) => {e.preventDefault(); setIsRegistering(false); }}
+                            >Login</a>
                             <p className="text-[18px] font-thin leading-[16px] ">I</p>
-                            <a href="#" className="font-medium text-[20px] leading-[16px]">Register</a>
+                            <a
+                                href="#"
+                                className={`font-medium text-[20px] leading-[16px] ${isRegistering ? "text-[#46A358]" : ""}`}
+                                onClick={(e) => {e.preventDefault(); setIsRegistering(true); }}
+                            >Register</a>
                         </div>
                         <div
                             className="flex flex-col gap-[20px] justify-center content-center ">
-                            <p className="text-[13px] leading-[16px] flex items-start">Enter
-                                your email and password to login.</p>
+                            <p className="text-[13px] leading-[16px] flex items-start">
+                                {isRegistering ? "Enter your email and" +
+                                    " password to register." : "Enter your" +
+                                    " email and password to login."} </p>
+
+                            {isRegistering && (
+                                <input type="text"
+                                       className="w-[300px] h-[40px] rounded-[5px] border"
+                                       style={{textIndent : "10px"}}
+                                       placeholder="Username"
+                                       required
+                                       autoComplete="username"/>
+                            )}
                             <input type="email"
                                    className="w-[300px] h-[40px] rounded-[5px] border "
-                                   placeholder="  qwerty_qwe@mail.ru" required
+                                   style={{textIndent : "10px"}}
+                                   placeholder={isRegistering ? "Enter" +
+                                       " your email address" : "Email"}
+                                   required
                                    autoComplete="email"/>
-                            <input type="password"
-                                   className="w-[300px] h-[40px] rounded-[5px] border relative content: '' absolute bg-[url(../assets/Header/Hide.png)]"
-                                   placeholder="  password" required
-                                   autoComplete="current-password"/>
-                            <a href="#"
-                               className="text-[14px] leading-[16px] text-[#46A358] flex justify-end">Forgot
-                                Password?</a>
+                            <div className="relative">
+                                <input type={isPasswordVisible ? "text" : "password"}
+                                       className="w-[300px] h-[40px] rounded-[5px] border relative content: '' absolute bg-[url(../assets/Header/Hide.png)]"
+                                       style={{textIndent: "10px"}}
+                                       placeholder="Password"
+                                       required
+                                       autoComplete="current-password"/>
+
+                                <button type="button"
+                                onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                                className="absolute top-1/2 right-3 transform -translate-y-1/2">
+                                    <img src={isPasswordVisible ? ImgShow : ImgHide} alt="Hide/Show" className="w-5 h-5"/>
+                                </button>
+                            </div>
+
+                            {isRegistering && (
+                                <div className="relative">
+                                    <input type={isConfirmPasswordVisible ? "text" : "password"}
+                                           className="w-[300px] h-[40px] rounded-[5px] border"
+                                           style={{textIndent: "10px"}}
+                                           placeholder="Confirm password"
+                                           required
+                                           autoComplete="new-password"/>
+
+                                    <button type="button"
+                                            onClick={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
+                                            className="absolute top-1/2 right-3 transform -translate-y-1/2">
+                                        <img
+                                            src={isConfirmPasswordVisible ? ImgShow : ImgHide}
+                                            alt="Hide/Show"
+                                            className="w-5 h-5"/>
+                                    </button>
+                                </div>
+                            )}
+
+                            {!isRegistering && (
+                                <a href="#"
+                                   className="text-[14px] leading-[16px] text-[#46A358] flex justify-end">Forgot
+                                    Password?</a>
+                            )}
+
                             <button type="submit"
                                     className="w-[300px] h-[45px] rounded-[5px] border bg-[#46A358] text-center text-white">
                                 <span
-                                    className="text-[16px] leading-[16px] font-bold">Login</span>
+                                    className="text-[16px] leading-[16px] font-bold">{isRegistering ? "Register" : "Login"}</span>
                             </button>
                         </div>
 
@@ -80,8 +139,7 @@ export default function Header() {
                             <div
                                 className="flex flex-row justify-between items-center ">
                                 <img src={ImgLine1} alt="Line1"/>
-                                <p className="text-[13px] leading-[16px] text-[#3D3D3D]">Or
-                                    login with </p>
+                                <p className="text-[13px] leading-[16px] text-[#3D3D3D]"> {isRegistering ? "Or continue with" : "Or login with"}  </p>
                                 <img src={ImgLine2} alt="Line2"/>
                             </div>
                             <button
@@ -89,17 +147,17 @@ export default function Header() {
                                 <img src={ImgGoogle} alt="Goggle-icon"
                                      className=" w-[20px] h-[20px] bg-cover bg-no-repeat "/>
                                 <span
-                                    className="text-[13px] leading-[16px] font-medium text-center text-[#727272]">Login with Google</span>
+                                    className="text-[13px] leading-[16px] font-medium text-center text-[#727272]">{isRegistering ? "Continue with Google" : "Login with Google"}</span>
                             </button>
                             <button
                                 className="w-[300px] h-[40px] rounded-[5px] border border-[#EAEAEA] bg-[#FFFFFF] flex flex-row items-center justify-center gap-[10px]">
                                 <img src={ImgFacebook} alt="Facebook-icon"
                                      className="w-[20px] h-[20px] bg-cover bg-no-repeat "/>
                                 <span
-                                    className="text-[13px] leading-[16px] font-medium text-center text-[#727272]">Login with Facebook</span>
+                                    className="text-[13px] leading-[16px] font-medium text-center text-[#727272]">{isRegistering ? "Continue with Facebook" : "Login with Facebook"}</span>
                             </button>
                         </div>
-                        <div className="w-full h-[10px] bg-[#46A358] relative bottom-[-20px]"></div>
+                        <div className="w-full h-[10px] bg-[#46A358] relative bottom-[-10px]"></div>
                     </Modal>
                 </div>
             </header>
--- frontend/plants/src/components/Modal.tsx
+++ frontend/plants/src/components/Modal.tsx
@@ -1,11 +1,18 @@
-const Modal = ({isOpen, closeModal, children}) => {
+import ImgX from "../assets/Header/X.svg";
+
+const Modal = ({isOpen, closeModal, children, isRegistering}) => {
     if(!isOpen){ return null }
 
     return (
         <div
             className="fixed w-full h-full bg-black/30 flex justify-center items-center z-10 right-0 ">
             <div
-                className="bg-white w-[500px] h-[600px] justify-center items-center text-center flex flex-col shadow-sm gap-[50px]">
+                className={'relative bg-white w-[500px] justify-center' +
+                    ' items-center text-center flex flex-col shadow-sm gap-[50px] transition-all duration-300' +
+                    `${isRegistering ? "h-[700px]" : "h-[600px]"}`}>
+                <button onClick={closeModal}
+                        className="absolute top-4 right-4">
+                    <img src={ImgX} alt="X"/></button>
                 {children}
             </div>
         </div>

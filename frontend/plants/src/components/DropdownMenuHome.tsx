17b5189147623b996ff563b1d13929a1088b8ac4
Kit <nikita.khaziev@mail.ru>
2025-03-19 10:56:41 +0300

    выпадающее меню сделано

--- frontend/plants/src/components/DropdownMenuHome.tsx
+++ frontend/plants/src/components/DropdownMenuHome.tsx
@@ -1,5 +1,5 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon, CheckIcon } from '@heroicons/react/20/solid'
 import {useState} from 'react'
 
 
@@ -26,30 +26,27 @@ export default function DropdownMenuHome() {
                 className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
                 <div className="py-1">
-                    <MenuItem>
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                        >
-                            Default sorting
-                        </a>
-                    </MenuItem>
-                    <MenuItem>
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                        >
-                            Sort Ascending
-                        </a>
-                    </MenuItem>
-                    <MenuItem>
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                        >
-                            Sort Descending
-                        </a>
-                    </MenuItem>
+                    {options.map((option) => (
+                        <MenuItem key={option}>
+                            {({active}) => (
+                                <a
+                                    href="#"
+                                    className={`block px-4 py-2 text-sm text-gray-700 ${
+                                        active ? 'bg-gray-100' : ''
+                                    }`}
+                                    onClick={() => setSelectedOption(option)}
+                                >
+                                    <div className="flex items-center">
+                                        <span className="flex-1">{option}</span>
+                                        {selectedOption === option && (
+                                            <CheckIcon className="h-5 w-5 text-green-500"/>
+                                        )}
+                                    </div>
+
+                                </a>
+                            )}
+                        </MenuItem>
+                    ))}
                 </div>
             </MenuItems>
         </Menu>
